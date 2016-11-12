ff9798b99f18a6ab98cf82d0e5e0400e1af6d649
Nantaya Khaongam <57160418@go.buu.ac.th>
2016-11-12 11:43:27 +0700

    edit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
- get 'static_pages/home'
- get 'static_pages/help'
- root 'application#hello'
-end
+   get 'static_pages/home'
+   get 'static_pages/help'
+   root 'application#hello' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +56,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+end
